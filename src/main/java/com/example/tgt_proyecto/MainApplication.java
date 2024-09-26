839b29322fa2ff858463db318c969f446abe7f45
juanMoreno <juanpablomorenolopez9@gmail.com>
2024-09-26 18:11:06 -0500

    Login Terminada

--- src/main/java/com/example/tgt_proyecto/MainApplication.java
+++ src/main/java/com/example/tgt_proyecto/MainApplication.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.geometry.Rectangle2D;
 
 import java.net.URL;
 
@@ -19,15 +21,25 @@ public class MainApplication extends Application {
                 System.out.println("No se pudo encontrar el archivo login-view.fxml");
                 return;
             }
+
             FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
-            Scene scene = new Scene(fxmlLoader.load(), 1007, 641);
+            Scene scene = new Scene(fxmlLoader.load());
 
             stage.setTitle("TGT | EQUIPMENTS");
-            stage.setMaximized(true);
+
+            // Obtener la resolución de pantalla actual
+            Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+            // Ajustar el tamaño de la ventana a la resolución de pantalla
+            stage.setWidth(screenBounds.getWidth());
+            stage.setHeight(screenBounds.getHeight());
+
             stage.setResizable(true);
 
+            // Establecer el icono del stage
             stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/tgt_proyecto/icons/logo.ico")));
 
+            // Mostrar la escena
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
